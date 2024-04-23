1a4435b3da5f2af0b5aeb610b92e615c2e1f9f71
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2024-04-24 01:11:06 +0200

    feat: Add testing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,67 @@
+import { useState } from "react";
 import { useIntl } from "react-intl";
+
+import { Button } from "./components/ui/button";
 
 function App() {
   const intl = useIntl();
 
+  const [count, setCount] = useState(0);
+  const [idk, setIdk] = useState<any[]>([]);
+
+  const handleClick = () => {
+    setCount((prev) => prev + 1);
+
+    setIdk((prev) => [
+      ...prev,
+      {
+        id: Math.random(),
+        top: Math.random() * 100,
+        left: Math.random() * 100,
+      },
+    ]);
+  };
+
   return (
-    <h1>
-      {intl.formatMessage({
-        id: "hello-world-test",
-        defaultMessage: "Hello, world!",
-      })}
-    </h1>
+    <>
+      <div className="absolute top-0 left-0 w-full h-full overflow-hidden -z-10">
+        {idk.map((item) => (
+          <h1
+            className="absolute text-4xl whitespace-nowrap"
+            key={item.id}
+            style={{ top: `${item.top}%`, left: `${item.left}%` }}
+          >
+            {intl.formatMessage({
+              id: "hello-world.title",
+              defaultMessage: "Hello, world!",
+            })}
+          </h1>
+        ))}
+      </div>
+
+      <div className="grid items-center h-full justify-items-center">
+        <div className="flex flex-col gap-6">
+          <h1 className="text-4xl">
+            {intl.formatMessage({
+              id: "hello-world.title",
+              defaultMessage: "Hello, world!",
+            })}
+          </h1>
+
+          <Button onClick={handleClick}>
+            {intl.formatMessage(
+              {
+                id: "hello-world.button",
+                defaultMessage: "You clicked {count} times",
+              },
+              {
+                count,
+              },
+            )}
+          </Button>
+        </div>
+      </div>
+    </>
   );
 }
 
