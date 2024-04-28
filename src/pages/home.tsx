413f1537781b2e19ec4b1a1468965277e2cf734c
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2024-04-28 19:16:45 +0200

    feat: Implement homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,70 +1,31 @@
-import { Banana, Bird } from "lucide-react";
-import { useState } from "react";
 import { useIntl } from "react-intl";
 
-import { Button } from "@/components/ui/button";
-
-type HelloHeadingInfo = {
-  id: number;
-  top: number;
-  left: number;
-};
+import { ImportQuestionsPrompt } from "@/components/forms";
 
 export function Home() {
   const intl = useIntl();
 
-  const [count, setCount] = useState(0);
-  const [floatingHelloes, setFloatingHelloes] = useState<HelloHeadingInfo[]>(
-    [],
-  );
-
-  const handleClick = () => {
-    setCount((prev) => prev + 1);
-
-    setFloatingHelloes((prev) => [
-      ...prev,
-      {
-        id: Math.random(),
-        top: Math.random() * 100,
-        left: Math.random() * 100,
-      },
-    ]);
-  };
-
   return (
     <>
-      <div className="absolute top-0 left-0 w-full h-full overflow-hidden -z-10">
-        {floatingHelloes.map((item) => (
-          <div
-            key={item.id}
-            className="absolute"
-            style={{ top: `${item.top}vh`, left: `${item.left}vw` }}
-          >
-            {intl.locale === "en" ? <Banana /> : <Bird />}
-          </div>
-        ))}
-      </div>
-
       <div className="grid items-center h-full justify-items-center">
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col items-center gap-6">
           <h1 className="text-4xl">
             {intl.formatMessage({
-              id: "hello-world.title",
-              defaultMessage: "Hello, world!",
-            })}
+              id: "homepage.title.welcome-to",
+              defaultMessage: "Welcome to",
+            })}{" "}
+            <span className="font-bold">PrepDrill</span>
           </h1>
 
-          <Button onClick={handleClick}>
-            {intl.formatMessage(
-              {
-                id: "hello-world.button",
-                defaultMessage: "You clicked {count} times",
-              },
-              {
-                count,
-              },
-            )}
-          </Button>
+          <h2 className="text-2xl">
+            {intl.formatMessage({
+              id: "homepage.subtitle",
+              defaultMessage:
+                "Simple application for drilling the questions for exams",
+            })}
+          </h2>
+
+          <ImportQuestionsPrompt />
         </div>
       </div>
     </>
