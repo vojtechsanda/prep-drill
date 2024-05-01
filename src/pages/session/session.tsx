07534d8b39a75efd23a04978f2cef4ccf5259961
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2024-05-01 15:49:23 +0200

    feat: Add session page

---
+++ src/pages/session/session.tsx
@@ -0,0 +1,3 @@
+export function Session() {
+  return <>Session</>;
+}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,19 @@
 import { Layout } from "@/components/layout";
 import { Home } from "@/pages/home";
+import { Session } from "@/pages/session";
 
 export function useRoutes() {
   return [
     {
       path: "/",
       element: <Layout />,
-      children: [{ path: "/", element: <Home /> }],
+      children: [
+        { path: "/", element: <Home /> },
+        {
+          path: "/session",
+          element: <Session />,
+        },
+      ],
     },
   ];
 }
