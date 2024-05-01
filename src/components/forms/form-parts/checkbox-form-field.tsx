8cc6d1e2e292caa2d304f14fc1a23505b5ea9589
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2024-05-01 18:48:19 +0200

    feat: Add checkbox

---
+++ src/components/forms/form-parts/checkbox-form-field.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { FieldValues, Path, useForm } from "react-hook-form";
+
+import { Checkbox, CheckboxProps } from "@/components/ui/checkbox";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { cn } from "@/lib/utils";
+
+type CheckboxFormField<T extends FieldValues> = Omit<
+  CheckboxProps,
+  "name" | "form" | "label"
+> & {
+  form: ReturnType<typeof useForm<T>>;
+  name: Path<T>;
+  label?: string;
+};
+
+export function CheckboxFormField<T extends FieldValues>({
+  form,
+  name,
+  label,
+  className,
+  ...props
+}: CheckboxFormField<T>) {
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel
+            className={cn(
+              "flex flex-row items-start p-4 space-x-3 space-y-0 border rounded-md cursor-pointer",
+              className,
+            )}
+          >
+            <FormControl>
+              <Checkbox
+                checked={field.value}
+                onCheckedChange={field.onChange}
+                {...props}
+              />
+            </FormControl>
+            <span>{label}</span>
+          </FormLabel>
+        </FormItem>
+      )}
+    />
+  );
+}
--- src/components/forms/form-parts/index.ts
+++ src/components/forms/form-parts/index.ts
@@ -2,3 +2,4 @@ export { FileFormField } from "./file-form-field";
 export { Form } from "./form";
 export { FormFieldInput } from "./form-field-input";
 export { SwitchFormField } from "./switch-form-field";
+export { CheckboxFormField } from "./checkbox-form-field";
