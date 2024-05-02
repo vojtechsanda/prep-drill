535d4b90f97c00017d05f1ca1f4ff0558a9cba5a
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2024-05-03 01:42:30 +0200

    fix: Fix footer padding collapsing

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -5,7 +5,7 @@ import { Menu } from "./menu";
 
 export function Layout() {
   return (
-    <div className="flex flex-col h-full px-4 py-2 mx-auto sm:py-3 sm:px-5 max-w-screen-2xl md:px-6 md:pt-4 lg:pt-6">
+    <div className="flex flex-col h-full p-4 pb-2 mx-auto sm:pb-3 min-h-min sm:p-5 max-w-screen-2xl md:px-6 md:pt-6">
       <Menu />
       <div className="flex-1">
         <div className="grid items-center h-full py-8 justify-items-center">
