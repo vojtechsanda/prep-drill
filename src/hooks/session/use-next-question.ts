464f93e320aa73a9f55197486ad3747beab779c4
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2024-05-03 01:24:47 +0200

    fix: Fix next question id computation

--- src/hooks/session/use-next-question.ts
+++ src/hooks/session/use-next-question.ts
@@ -1,27 +1,23 @@
-import { useSavedQuestionsQuery } from "@/hooks/storage/questions";
 import { useSaveSessionMutation } from "@/hooks/storage/session";
 
 import { useSessionInfo } from "./use-session-info";
 
 export function useNextQuestion() {
   const sessionInfo = useSessionInfo();
-  const questionsQuery = useSavedQuestionsQuery();
 
   const { mutateAsync: saveSession } = useSaveSessionMutation();
 
   return async () => {
-    const questions = questionsQuery.data;
-    if (!sessionInfo || !questions?.length) return;
+    if (!sessionInfo) return;
 
-    const currentQuestionIndex = questions.findIndex(
-      (question) => question.id === sessionInfo.currentQuestion?.id,
+    const currentQuestionIndex = sessionInfo.session.questionsIds.indexOf(
+      sessionInfo.session.currentQuestionId ?? "",
     );
 
     await saveSession({
       ...sessionInfo.session,
-      currentQuestionId: currentQuestionIndex
-        ? questions[currentQuestionIndex + 1]?.id
-        : null,
+      currentQuestionId:
+        sessionInfo.session.questionsIds[currentQuestionIndex + 1] ?? null,
     });
   };
 }
