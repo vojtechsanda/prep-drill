b3a46a4fa3a1549affaec73c5c50626c4f3e603d
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2024-05-02 03:02:43 +0200

    feat: Add back to session button on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,11 @@
+import { ArrowBigDownDash, GraduationCap } from "lucide-react";
 import { useIntl } from "react-intl";
+import { Link } from "react-router-dom";
 
 import { ImportQuestionsPrompt } from "@/components/forms";
 import { SetupSessionPrompt } from "@/components/forms/setup-session-prompt";
 import { Button } from "@/components/ui/button";
+import { useSessionInfo } from "@/hooks/session";
 import { useSavedQuestionsQuery } from "@/hooks/storage/questions";
 
 export function Home() {
@@ -11,9 +14,13 @@ export function Home() {
   const questionsQuery = useSavedQuestionsQuery();
   const questions = questionsQuery.data ?? [];
 
+  const sessionInfo = useSessionInfo();
+
   if (questionsQuery.isLoading) {
     return null;
   }
+
+  const sessionIsRunning = sessionInfo && !sessionInfo.isFinished;
 
   return (
     <div className="flex flex-col items-center gap-6">
@@ -35,7 +42,29 @@ export function Home() {
       <div className="flex flex-col items-center gap-2">
         {!!questions.length && (
           <div className="flex items-center gap-4">
-            <SetupSessionPrompt />
+            <SetupSessionPrompt>
+              {sessionIsRunning ? (
+                <Button>
+                  <GraduationCap />
+                  {intl.formatMessage({
+                    id: "homepage.setup-session",
+                    defaultMessage: "Start new drill",
+                  })}
+                </Button>
+              ) : null}
+            </SetupSessionPrompt>
+
+            {sessionIsRunning && (
+              <Button asChild>
+                <Link to="/session">
+                  <ArrowBigDownDash />
+                  {intl.formatMessage({
+                    id: "homepage.back-to-session",
+                    defaultMessage: "Back to session",
+                  })}
+                </Link>
+              </Button>
+            )}
           </div>
         )}
         <ImportQuestionsPrompt>
