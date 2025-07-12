318eeba52b67ddf28a9027d1deb2a18786e10e26
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2025-07-12 20:37:13 +0200

    feat: Add name to footer note

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+
 import { cn } from "@/lib/utils";
 
 export function Footer({ className }: { className?: string }) {
   return (
     <p className={cn("text-sm text-center", className)}>
-      Made with 💖 in Czech Republic
+      Made with 💖 by{" "}
+      <Link to="https://vojtechsanda.cz" className="underline" target="_blank">
+        Vojtěch Šanda
+      </Link>
     </p>
   );
 }
